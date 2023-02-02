84ac4144e82d684f313a1618bf519a86dacbd196
Marcela Silva <110864304+Marci-Hime@users.noreply.github.com>
2023-02-02 07:48:31 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+class Main {
+  public static void main(String[] args) {
+    
+    Scanner entrada = new Scanner(System.in);
+        
+    Jogador j1 = new Jogador();
+    Dado d1 = new Dado();
+    SistemaAzar s1 = new SistemaAzar();
+
+    System.out.println("Insira sua aposta!");
+    d1.setFace(entrada.nextInt());
+    
+    System.out.println("Insira seu nome!");
+    j1.setNome(entrada.next());
+    j1.nomeado();
+
+    s1.setJogador(j1);
+    s1.setDado(d1);
+    d1.sorteado();
+    s1.verificarGanhador();
+    
+    
+  }
+}
