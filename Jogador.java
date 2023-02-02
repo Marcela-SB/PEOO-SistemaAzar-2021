c9f0a72eab560642d977d61af08048a930842862
Marcela Silva <110864304+Marci-Hime@users.noreply.github.com>
2023-02-02 07:51:42 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -1,1 +1,20 @@
+public class Jogador{
+  private String nome;
+  private int aposta;
 
+  public String getNome(){
+    return this.nome;
+  }
+  public void setNome(String nome){
+    this.nome=nome;
+  }
+  public int getAposta(){
+    return this.aposta;
+  }
+  public void setAposta(int aposta){
+    this.aposta=aposta;
+  }
+  public void nomeado(){
+    System.out.println("Nome do jogador: "+getNome());
+  }
+}
