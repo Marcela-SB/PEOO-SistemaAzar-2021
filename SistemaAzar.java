51caa58e87abba9bb45d534b9e6b3b4b1ae024c7
Marcela Silva <110864304+Marci-Hime@users.noreply.github.com>
2023-02-02 07:53:50 -0300

    Update SistemaAzar.java

--- SistemaAzar.java
+++ SistemaAzar.java
@@ -1,1 +1,27 @@
+import java.util.Random;
 
+public class SistemaAzar{
+  Jogador jogador;
+  Dado dado;
+
+  public Jogador getJogador(){
+    return this.jogador;
+  }
+  public void setJogador(Jogador jogador){
+    this.jogador=jogador;
+  }
+  public Dado getDado(){
+    return this.dado;
+  }
+  public void setDado(Dado dado){
+    this.dado=dado;
+  }
+  public void verificarGanhador(){
+    if(jogador.getAposta() == dado.getFace()){
+      System.out.println("Parabéns!");
+    }
+    else{
+      System.out.println("Melhor na próxima!");
+    }
+  }
+}
